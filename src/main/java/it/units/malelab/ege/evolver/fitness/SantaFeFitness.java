aa1338736f9513fb0f7acd1d4a8ba244ee50800f
Danny <Danny@172.30.42.118>
2016-10-10 15:53:20 +0200

    Fixed csv header Fixed SantaFe fitness for multi-thread application

--- src/main/java/it/units/malelab/ege/evolver/fitness/SantaFeFitness.java
+++ src/main/java/it/units/malelab/ege/evolver/fitness/SantaFeFitness.java
@@ -9,9 +9,6 @@ import java.util.List;
  */
 public class SantaFeFitness implements FitnessComputer<String> {
 
-    private Trail trail;
-    private List<Node<String>> phenotype;
-    private int programCounter;
     static final String IF = "if(food_ahead()){";
     static final String ELSE = "}else{";
     static final String END_IF = "}";
@@ -21,14 +18,13 @@ public class SantaFeFitness implements FitnessComputer<String> {
 
     @Override
     public Fitness compute(Node<String> phenotypeRoot) {
-        phenotype = phenotypeRoot.leaves();
-        trail = new Trail(); //Trail to travese
+        List<Node<String>> phenotype = phenotypeRoot.leaves();
+        Trail trail = new Trail(); //Trail to travese
         double fitness = trail.getFood(); //Initial fitness
         try {
             if (phenotype.size() > 0) {
                 while (trail.get_Energy() > 0) {
-                    this.programCounter = 0;
-                    run(phenotype);
+                    run(phenotype, trail, 0);
                 }
                 fitness = trail.getFitness();
             } else {
@@ -45,37 +41,41 @@ public class SantaFeFitness implements FitnessComputer<String> {
         return new NumericFitness(Double.POSITIVE_INFINITY);
     }
 
-    private void lookAheadElse(List<Node<String>> phenotype) {
+    private void lookAheadElse(List<Node<String>> phenotype, int programCounter) {
         boolean found = false;
         int depth = 0;//Keep track of nested ifs
         //While program not finished and the else bloch for depth 0 is found
-        while (phenotype.size() > this.programCounter && !found) {
+        while (phenotype.size() > programCounter && !found) {
             //Current token
-            final String token = phenotype.get(this.programCounter).getContent();
+            final String token = phenotype.get(programCounter).getContent();
             //Check else statment and correct depth
             if (token.equals(SantaFeFitness.ELSE) && depth == 0) {
                 found = true; //Found
             } else //If staement increase depth
-             if (token.equals(SantaFeFitness.IF)) {
+            {
+                if (token.equals(SantaFeFitness.IF)) {
                     depth++;
                 } else //End of if statemnt decrease depth
-                 if (token.equals(SantaFeFitness.END_IF)) {
+                {
+                    if (token.equals(SantaFeFitness.END_IF)) {
                         depth--;
                     }
-            this.programCounter++;
+                }
+            }
+            programCounter++;
         }
     }
 
     /**
      * Find the end if statement
      */
-    private void lookAheadEnd_If(List<Node<String>> phenotype) {
+    private void lookAheadEnd_If(List<Node<String>> phenotype, int programCounter) {
         boolean found = false;
         int depth = 0;//Keep track of nested ifs
         //While program not finished and the else bloch for depth 0 is found
-        while (phenotype.size() > this.programCounter && !found) {
+        while (phenotype.size() > programCounter && !found) {
             //Current token
-            final String token = phenotype.get(this.programCounter).getContent();
+            final String token = phenotype.get(programCounter).getContent();
             //Check else statment and correct depth
             if (token.equals(SantaFeFitness.END_IF)) {
                 if (depth == 0) {
@@ -84,40 +84,42 @@ public class SantaFeFitness implements FitnessComputer<String> {
                     depth--;
                 }
             } else //If staement increase depth
-             if (token.equals(SantaFeFitness.IF)) {
+            {
+                if (token.equals(SantaFeFitness.IF)) {
                     depth++;
                 }
-            this.programCounter++;
+            }
+            programCounter++;
         }
     }
 
     /**
      * Execute the program by calling the functions in Trail
      */
-    private void run(List<Node<String>> phenotype) {
+    private void run(List<Node<String>> phenotype, Trail trail, int programCounter) {
         //Check if end of program
-        if (this.programCounter < phenotype.size()) {
+        if (programCounter < phenotype.size()) {
             //Get current token
-            final String token = phenotype.get(this.programCounter).getContent();
+            final String token = phenotype.get(programCounter).getContent();
             //Increase program counter
-            this.programCounter++;
+            programCounter++;
             if (token.equals(SantaFeFitness.IF)) {//IF food ahead
-                if (this.trail.food_ahead() != 1) {
+                if (trail.food_ahead() != 1) {
                     //Find else statement
-                    lookAheadElse(phenotype);
+                    lookAheadElse(phenotype, programCounter);
                 }
             } else if (token.equals(SantaFeFitness.LEFT)) {//left
-                this.trail.left();
+                trail.left();
             } else if (token.equals(SantaFeFitness.MOVE)) {//move
-                this.trail.move();
+                trail.move();
             } else if (token.equals(SantaFeFitness.RIGHT)) {//right
-                this.trail.right();
+                trail.right();
             } else if (token.equals(SantaFeFitness.ELSE)) {
-                lookAheadEnd_If(phenotype);
+                lookAheadEnd_If(phenotype, programCounter);
             } else if (!token.equals(SantaFeFitness.END_IF)) {
                 throw new IllegalArgumentException("Illegal Terminal symbol:" + token);
             }
-            run(phenotype);//countinue executing program
+            run(phenotype, trail, programCounter);//countinue executing program
         }
     }
 }
