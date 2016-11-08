29cf697e1620dbe556e0a3ec8b3860bac377e62c
Danny <Danny@172.30.42.118>
2016-11-08 16:02:01 +0100

    added SGE mapper with its operators (crossover and mutation)

--- src/main/java/it/units/malelab/ege/TestDistances.java
+++ src/main/java/it/units/malelab/ege/TestDistances.java
@@ -10,10 +10,13 @@ import it.units.malelab.ege.distance.BitsGenotypeEditDistance;
 import it.units.malelab.ege.distance.CachedDistance;
 import it.units.malelab.ege.distance.Distance;
 import it.units.malelab.ege.distance.EditDistance;
+import it.units.malelab.ege.distance.SGEGenotypeHammingDistance;
 import it.units.malelab.ege.distance.TreeEditDistance;
 import it.units.malelab.ege.evolver.fitness.FitnessComputer;
 import it.units.malelab.ege.evolver.genotype.BitsGenotype;
 import it.units.malelab.ege.evolver.genotype.BitsGenotypeFactory;
+import it.units.malelab.ege.evolver.genotype.Genotype;
+import it.units.malelab.ege.evolver.genotype.SGEGenotypeFactory;
 import it.units.malelab.ege.evolver.initializer.RandomInitializer;
 import it.units.malelab.ege.evolver.operator.AbstractCrossover;
 import it.units.malelab.ege.evolver.operator.AbstractMutation;
@@ -24,6 +27,8 @@ import it.units.malelab.ege.evolver.operator.LengthPreservingOnePointCrossover;
 import it.units.malelab.ege.evolver.operator.LengthPreservingTwoPointsCrossover;
 import it.units.malelab.ege.evolver.operator.OnePointCrossover;
 import it.units.malelab.ege.evolver.operator.ProbabilisticMutation;
+import it.units.malelab.ege.evolver.operator.SGECrossover;
+import it.units.malelab.ege.evolver.operator.SGEMutation;
 import it.units.malelab.ege.evolver.operator.TwoPointsCrossover;
 import it.units.malelab.ege.evolver.validator.AnyValidator;
 import it.units.malelab.ege.grammar.Grammar;
@@ -33,6 +38,7 @@ import it.units.malelab.ege.mapper.HierarchicalMapper;
 import it.units.malelab.ege.mapper.Mapper;
 import it.units.malelab.ege.mapper.MappingException;
 import it.units.malelab.ege.mapper.PiGEMapper;
+import it.units.malelab.ege.mapper.SGEMapper;
 import it.units.malelab.ege.mapper.StandardGEMapper;
 import it.units.malelab.ege.mapper.WeightedHierarchicalMapper;
 import java.io.IOException;
@@ -54,8 +60,9 @@ import java.util.concurrent.ExecutionException;
 public class TestDistances {
 
     public static void main(String[] args) throws IOException, InterruptedException, ExecutionException {
-        Map<String, Distance<BitsGenotype>> genotypeDistances = new LinkedHashMap<>();
+        Map<String, Distance> genotypeDistances = new LinkedHashMap<>();
         genotypeDistances.put("BitsEdit", new CachedDistance<>(new BitsGenotypeEditDistance()));
+        genotypeDistances.put("SGEHamming", new CachedDistance<>(new SGEGenotypeHammingDistance()));
         Map<String, Distance<Node<String>>> phenotypeDistances = new LinkedHashMap<>();
         Map<String, String> descriptions = new LinkedHashMap<>();
         final EditDistance<String> editDistance = new EditDistance<>();
@@ -70,7 +77,7 @@ public class TestDistances {
         phenotypeDistances.put("TreeEdit", new CachedDistance<>(new TreeEditDistance<String>()));
         //prepare file
         PrintStream distancesFilePS = new PrintStream("dist" + dateForFile() + ".csv");
-        distancesFilePS.println("Distance;Problem;Mapper;Operator;GenoSize;ChildSize;p1-c_G;p1-c_P;p1-c_F;p2-c_G;p2-c_P;p2-c_F;p1-p2_G;p1-p2_P;p1-p2_F;c_PSize;c_PDepth;c_PLength;p1_PSize;p1_PDepth;p1_PLength;p2_PSize;p2_PDepth;p2_PLength");
+        distancesFilePS.println("Problem;Mapper;Operator;GenoSize;ChildSize;p1-c_G;p1-c_PL;p1-c_PT;p1-c_F;p2-c_G;p2-c_PL;p2-c_PT;p2-c_F;p1-p2_G;p1-p2_PL;p1-p2_PT;p1-p2_F;c_PSize;c_PDepth;c_PLength;p1_PSize;p1_PDepth;p1_PLength;p2_PSize;p2_PDepth;p2_PLength");
         //prepare problems
         Map<String, BenchmarkProblems.Problem> problems = new LinkedHashMap<>();
         problems.put("harmonic", BenchmarkProblems.harmonicCurveProblem());
@@ -81,47 +88,70 @@ public class TestDistances {
         Mapper mapper;
         AbstractOperator operator;
         int n_individuals = 300;
-        List[] genosSet1 = new List[5];
-        List[] genosSet2 = new List[5];
-        for (int i = 0; i < genosSet1.length; i++) {
-            genosSet1[i] = (new RandomInitializer<>(new Random(i), new BitsGenotypeFactory((int) (128 * Math.pow(2, i))))).getGenotypes(n_individuals, new AnyValidator());
-            genosSet2[i] = (new RandomInitializer<>(new Random(i + 5), new BitsGenotypeFactory((int) (128 * Math.pow(2, i))))).getGenotypes(n_individuals, new AnyValidator());
+        List[] bitGenosSet1 = new List[5];
+        List[] bitGenosSet2 = new List[5];
+        List genosSet1 = null, genosSet2 = null;
+        for (int i = 0; i < bitGenosSet1.length; i++) {
+            bitGenosSet1[i] = (new RandomInitializer<>(new Random(i), new BitsGenotypeFactory((int) (128 * Math.pow(2, i))))).getGenotypes(n_individuals, new AnyValidator());
+            bitGenosSet2[i] = (new RandomInitializer<>(new Random(i + 5), new BitsGenotypeFactory((int) (128 * Math.pow(2, i))))).getGenotypes(n_individuals, new AnyValidator());
         }
-        for (String distancePName : phenotypeDistances.keySet()) {
-            descriptions.put("distanceName", distancePName);
-            for (String problemName : problems.keySet()) {
-                descriptions.put("problemName", problemName);
-                BenchmarkProblems.Problem problem = problems.get(problemName);
-                for (int m = 0; m < 6; m++) {
-                    mapper = null;
-                    Random r = new Random(m);
-                    Grammar<String> grammar = problem.getGrammar();
-                    switch (m) {
-                        case 0:
-                            descriptions.put("mapperName", "StdGE");
-                            mapper = new StandardGEMapper<>(8, 5, grammar);
-                            break;
-                        case 1:
-                            descriptions.put("mapperName", "BreathFirst");
-                            mapper = new BreathFirstMapper<>(8, 5, grammar);
-                            break;
-                        case 2:
-                            descriptions.put("mapperName", "PiGE");
-                            mapper = new PiGEMapper<>(16, 5, grammar);
-                            break;
-                        case 3:
-                            descriptions.put("mapperName", "BitsSGE");
-                            mapper = new BitsSGEMapper<>(6, grammar);
-                            break;
-                        case 4:
-                            descriptions.put("mapperName", "Hier");
-                            mapper = new HierarchicalMapper<>(grammar);
-                            break;
-                        case 5:
-                            descriptions.put("mapperName", "wHier");
-                            mapper = new WeightedHierarchicalMapper<>(6, grammar);
-                            break;
+        for (String problemName : problems.keySet()) {
+            descriptions.put("problemName", problemName);
+            BenchmarkProblems.Problem problem = problems.get(problemName);
+            for (int m = 0; m < 7; m++) {
+                mapper = null;
+                Random r = new Random(m);
+                Grammar<String> grammar = problem.getGrammar();
+                switch (m) {
+                    case 0:
+                        descriptions.put("mapperName", "StdGE");
+                        mapper = new StandardGEMapper<>(8, 5, grammar);
+                        break;
+                    case 1:
+                        descriptions.put("mapperName", "BreathFirst");
+                        mapper = new BreathFirstMapper<>(8, 5, grammar);
+                        break;
+                    case 2:
+                        descriptions.put("mapperName", "PiGE");
+                        mapper = new PiGEMapper<>(16, 5, grammar);
+                        break;
+                    case 3:
+                        descriptions.put("mapperName", "BitsSGE");
+                        mapper = new BitsSGEMapper<>(6, grammar);
+                        break;
+                    case 4:
+                        descriptions.put("mapperName", "SGE");
+                        mapper = new SGEMapper<>(6, grammar);
+                        genosSet1 = (new RandomInitializer<>(new Random(1), new SGEGenotypeFactory((SGEMapper) mapper))).getGenotypes(n_individuals, new AnyValidator());
+                        genosSet2 = (new RandomInitializer<>(new Random(2), new SGEGenotypeFactory((SGEMapper) mapper))).getGenotypes(n_individuals, new AnyValidator());
+                        break;
+                    case 5:
+                        descriptions.put("mapperName", "Hier");
+                        mapper = new HierarchicalMapper<>(grammar);
+                        break;
+                    case 6:
+                        descriptions.put("mapperName", "wHier");
+                        mapper = new WeightedHierarchicalMapper<>(6, grammar);
+                        break;
+                }
+                
+                if (mapper.getClass().equals(SGEMapper.class)) {
+                    for (int o = 0; o < 2; o++) {
+                        operator = null;
+                        switch (o) {
+                            case 0:
+                                operator = new SGECrossover(r);
+                                descriptions.put("operatorName", "SGECrossover");
+                                break;
+                            case 1:
+                                operator = new SGEMutation(0.01, (SGEMapper) mapper, r);
+                                descriptions.put("operatorName", "SGEMutation");
+                                break;
+                        }
+                        calcDistances(genosSet1, genosSet2, mapper, operator, distancesFilePS, genotypeDistances.get("SGEHamming"), phenotypeDistances, problem, descriptions);
+                        System.out.printf("%s %s %s\n", descriptions.get("problemName"), descriptions.get("mapperName"), descriptions.get("operatorName"));
                     }
+                } else {
                     for (int o = 0; o < 7; o++) {
                         operator = null;
                         switch (o) {
@@ -157,11 +187,10 @@ public class TestDistances {
                                 break;
                         }
                         if (operator != null && mapper != null) {
-                            for (int i = 0; i < genosSet1.length; i++) {
-                                //System.out.println(((BitsGenotype)genosSet1[i].get(0)).size());
-                                calcDistances(genosSet1[i], genosSet2[i], mapper, operator, distancesFilePS, genotypeDistances.get("BitsEdit"), phenotypeDistances.get(distancePName), problem, descriptions);
+                            for (int i = 0; i < bitGenosSet1.length; i++) {
+                                calcDistances(bitGenosSet1[i], bitGenosSet2[i], mapper, operator, distancesFilePS, genotypeDistances.get("BitsEdit"), phenotypeDistances, problem, descriptions);
                             }
-                            System.out.printf("%s %s %s %s\n", descriptions.get("distanceName"), descriptions.get("problemName"), descriptions.get("mapperName"), descriptions.get("operatorName"));
+                            System.out.printf("%s %s %s\n", descriptions.get("problemName"), descriptions.get("mapperName"), descriptions.get("operatorName"));
                         }
                     }
                 }
@@ -176,7 +205,7 @@ public class TestDistances {
         return simpleDateFormat.format(new Date());
     }
 
-    private static Node mapApply(Mapper mapper, BitsGenotype geno) {
+    private static Node mapApply(Mapper mapper, Genotype geno) {
         try {
             return mapper.map(geno);
         } catch (MappingException ex) {
@@ -200,8 +229,8 @@ public class TestDistances {
         }
     }
 
-    private static void calcDistances(List p1Set, List p2Set, Mapper mapper, AbstractOperator operator, PrintStream out, Distance<BitsGenotype> genoDist, Distance<Node<String>> phenoDist, Problem problem, Map<String, String> descr) {
-        BitsGenotype cG, p1G, p2G;
+    private static void calcDistances(List p1Set, List p2Set, Mapper mapper, AbstractOperator operator, PrintStream out, Distance genoDist, Map<String, Distance<Node<String>>> phenoDistSet, Problem problem, Map<String, String> descr) {
+        Genotype cG, p1G, p2G;
         Node cP, p1P, p2P;
         Double[] distArray;
         Integer[] sizes;
@@ -209,15 +238,16 @@ public class TestDistances {
         if (operator instanceof AbstractMutation) {
             for (int i = 0; i < p1Set.size(); i++) {
                 try {
-                    distArray = new Double[9];
+                    distArray = new Double[12];
                     sizes = new Integer[9];
+                    
                     /* array with the distances:
-                                                |     p1-c    |    p2-c     |    p1-p2    |
-                                                |  G   P   F  |  G   P   F  |  G   P   F  |
+                                                |        p1-c         |        p2-c         |        p1-p2        |
+                                                |  G   P(L) P(T)   F  |  G   P(L) P(T)   F  |  G   P(L) P(T)   F  |
                      */
 
-                    p1G = (BitsGenotype) p1Set.get(i);
-                    cG = (BitsGenotype) operator.apply(Arrays.asList(p1G)).get(0);
+                    p1G = (Genotype) p1Set.get(i);
+                    cG = (Genotype) operator.apply(Arrays.asList(p1G)).get(0);
                     distArray[0] = genoDist.d(p1G, cG);
                     p1P = mapApply(mapper, p1G);
                     cP = mapApply(mapper, cG);
@@ -231,17 +261,16 @@ public class TestDistances {
                         sizes[4] = p1P.depth();
                         sizes[5] = p1P.leaves().size();
                     }
-                    distArray[1] = distPhenoCalc(phenoDist, p1P, cP);
-                    distArray[2] = distFitCalc(problem.getFitnessComputer(), p1P, cP);
-                    //out.printf("%s\n%s\n%s\n%s\n",p1G.toInt(),cG.toInt(),p1P.leaves().toString(),cP.leaves().toString());
-                    out.printf("%s;%s;%s;%s;%d;%d;%f;%f;%f;%s;%s;%s;%s;%s;%s;%d;%d;%d;%d;%d;%d;%s;%s;%s\n",
-                            descr.get("distanceName"),
+                    distArray[1] = distPhenoCalc(phenoDistSet.get("LeavesEdit"), p1P, cP);
+                    distArray[2] = distPhenoCalc(phenoDistSet.get("TreeEdit"), p1P, cP);
+                    distArray[3] = distFitCalc(problem.getFitnessComputer(), p1P, cP);
+                    out.printf("%s;%s;%s;%d;%d;%f;%f;%f;%f;%s;%s;%s;%s;%s;%s;%s;%s;%d;%d;%d;%d;%d;%d;%s;%s;%s\n",
                             descr.get("problemName"),
                             descr.get("mapperName"),
                             descr.get("operatorName"),
                             p1G.size(),
                             cG.size(),
-                            distArray[0], distArray[1], distArray[2], "", "", "", "", "", "",
+                            distArray[0], distArray[1], distArray[2], distArray[3], "", "", "", "", "", "", "", "",
                             sizes[0], sizes[1], sizes[2], sizes[3], sizes[4], sizes[5], "", "", ""
                     );
                 } catch (ArrayIndexOutOfBoundsException ex) {
@@ -251,19 +280,20 @@ public class TestDistances {
         } else if (operator instanceof AbstractCrossover) {
             for (int i = 0; i < p1Set.size(); i++) {
                 try {
-                    distArray = new Double[9];
+                    distArray = new Double[12];
                     sizes = new Integer[9];
+                    
                     /* array with the distances:
-                                                |     p1-c    |    p2-c     |    p1-p2    |
-                                                |  G   P   F  |  G   P   F  |  G   P   F  |
+                                                |        p1-c         |        p2-c         |        p1-p2        |
+                                                |  G   P(L) P(T)   F  |  G   P(L) P(T)   F  |  G   P(L) P(T)   F  |
                      */
-
-                    p1G = (BitsGenotype) p1Set.get(i);
-                    p2G = (BitsGenotype) p2Set.get(i);
-                    cG = (BitsGenotype) operator.apply(Arrays.asList(p1G, p2G)).get(0);
+                    
+                    p1G = (Genotype) p1Set.get(i);
+                    p2G = (Genotype) p2Set.get(i);
+                    cG = (Genotype) operator.apply(Arrays.asList(p1G, p2G)).get(0);
                     distArray[0] = genoDist.d(p1G, cG);
-                    distArray[3] = genoDist.d(p2G, cG);
-                    distArray[6] = genoDist.d(p1G, p2G);
+                    distArray[4] = genoDist.d(p2G, cG);
+                    distArray[8] = genoDist.d(p1G, p2G);
                     cP = mapApply(mapper, cG);
                     p1P = mapApply(mapper, p1G);
                     p2P = mapApply(mapper, p2G);
@@ -282,21 +312,22 @@ public class TestDistances {
                         sizes[7] = p2P.depth();
                         sizes[8] = p2P.leaves().size();
                     }
-                    distArray[1] = distPhenoCalc(phenoDist, p1P, cP);
-                    distArray[4] = distPhenoCalc(phenoDist, p2P, cP);
-                    distArray[7] = distPhenoCalc(phenoDist, p1P, p2P);
-                    distArray[2] = distFitCalc(problem.getFitnessComputer(), p1P, cP);
-                    distArray[5] = distFitCalc(problem.getFitnessComputer(), p2P, cP);
-                    distArray[8] = distFitCalc(problem.getFitnessComputer(), p1P, p2P);
-                    //out.printf("%s\n%s\n%s\n%s\n%s\n%s\n",p1G.toInt(),p2G.toInt(),cG.toInt(),p1P.leaves().toString(),p2P.leaves().toString(),cP.leaves().toString());
-                    out.printf("%s;%s;%s;%s;%d;%d;%f;%f;%f;%f;%f;%f;%f;%f;%f;%d;%d;%d;%d;%d;%d;%d;%d;%d\n",
-                            descr.get("distanceName"),
+                    distArray[1] = distPhenoCalc(phenoDistSet.get("LeavesEdit"), p1P, cP);
+                    distArray[5] = distPhenoCalc(phenoDistSet.get("LeavesEdit"), p2P, cP);
+                    distArray[9] = distPhenoCalc(phenoDistSet.get("LeavesEdit"), p1P, p2P);
+                    distArray[2] = distPhenoCalc(phenoDistSet.get("TreeEdit"), p1P, cP);
+                    distArray[6] = distPhenoCalc(phenoDistSet.get("TreeEdit"), p2P, cP);
+                    distArray[10] = distPhenoCalc(phenoDistSet.get("TreeEdit"), p1P, p2P);
+                    distArray[3] = distFitCalc(problem.getFitnessComputer(), p1P, cP);
+                    distArray[7] = distFitCalc(problem.getFitnessComputer(), p2P, cP);
+                    distArray[11] = distFitCalc(problem.getFitnessComputer(), p1P, p2P);
+                    out.printf("%s;%s;%s;%d;%d;%f;%f;%f;%f;%f;%f;%f;%f;%f;%f;%f;%f;%d;%d;%d;%d;%d;%d;%d;%d;%d\n",
                             descr.get("problemName"),
                             descr.get("mapperName"),
                             descr.get("operatorName"),
                             p1G.size(),
                             cG.size(),
-                            distArray[0], distArray[1], distArray[2], distArray[3], distArray[4], distArray[5], distArray[6], distArray[7], distArray[8],
+                            distArray[0], distArray[1], distArray[2], distArray[3], distArray[4], distArray[5], distArray[6], distArray[7], distArray[8], distArray[9], distArray[10], distArray[11],
                             sizes[0], sizes[1], sizes[2], sizes[3], sizes[4], sizes[5], sizes[6], sizes[7], sizes[8]
                     );
                 } catch (ArrayIndexOutOfBoundsException ex) {
